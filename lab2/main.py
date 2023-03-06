c82a530c4e07b28233725aac775e18c5cd3a7ca3
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-03-06 16:50:46 +0100

    Dodanie zamieniania wielkości liter

--- lab2/main.py
+++ lab2/main.py
@@ -2,13 +2,24 @@
 
 import sys
 
+def invert(param):
+    ret = ""
+    for i in range(len(param)):
+        if param[i].islower():
+            ret = ret + param[i].upper()
+        else:
+            ret = ret + param[i].lower()
+    
+    return ret
+
+
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+                print(invert(param),end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
