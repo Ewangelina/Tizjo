ec67b800246ecc5c1d12cee37be19c2233964540
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-04-03 16:19:05 +0200

    stworzenie funkcji

--- lab5/main.py
+++ lab5/main.py
@@ -7,18 +7,22 @@ def number_change(x)
         return "9"
     elif x == "1":
         return "0"
-    elif x == "1":
-        return "0"
-    elif x == "1":
-        return "0"
-    elif x == "1":
-        return "0"
-    elif x == "1":
-        return "0"
-    elif x == "1":
-        return "0"
-    elif x == "1":
-        return "0"
+    elif x == "2":
+        return "1"
+    elif x == "3":
+        return "2"
+    elif x == "4":
+        return "3"
+    elif x == "5":
+        return "4"
+    elif x == "6":
+        return "5"
+    elif x == "7":
+        return "6"
+    elif x == "8":
+        return "7"
+    elif x == "9":
+        return "8"
 
 def is_number(x):
     return x.isnumeric()
