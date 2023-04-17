2c3567ca1e644b13305889d4bab29a1c1f94b6cb
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-04-17 16:23:40 +0200

    Change number change

--- lab6/main.py
+++ lab6/main.py
@@ -3,27 +3,10 @@
 import sys
 
 def number_change(x):
-    if x == "0":
-        return "9"
-    elif x == "1":
-        return "0"
-    elif x == "2":
-        return "1"
-    elif x == "3":
-        return "2"
-    elif x == "4":
-        return "3"
-    elif x == "5":
-        return "4"
-    elif x == "6":
-        return "5"
-    elif x == "7":
-        return "6"
-    elif x == "8":
-        return "7"
-    elif x == "9":
-        return "8"
-    else:
+    try:
+        num = int(x)
+        return string((x * 9 % 10))
+    except:
         return x
 
 def is_number(x):
