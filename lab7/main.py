f787eaa12a4ca15a7bbd9a94e5cd331077883893
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-04-24 16:13:54 +0200

    Change main

--- lab7/main.py
+++ lab7/main.py
@@ -2,57 +2,8 @@
 
 import sys
 
-def is_number(x):
-    return x.isnumeric()
-
-def invert_numbers(param):
-    ret = ""
-    nums = ""
-    skip = 0
-    
-    for i in range(len(param)):
-        while is_number(param[i + skip]):
-            nums = param[i + skip] + nums
-            skip += 1
-            if i + skip >= len(param):
-                return ret + nums
-            
-        if nums != "":
-            ret = ret + nums
-            nums = ""
-            
-        if skip == 0:
-            ret = ret + param[i]
-        else:
-            skip -= 1
-    return ret
-
-def invert_lengthen(param, min_length):
-    ret = ""
-    for i in range(len(param)):
-        if param[i].islower():
-            ret = ret + param[i].upper()
-        else:
-            ret = ret + param[i].lower()
-            
-    while len(ret) < min_length:
-        ret = " " + ret
-    
-    return ret
-
-def invert_shorten(param, max_length):
-    ret = ""
-    for i in range(max_length):
-    	if i > len(param):
-            break
-            
-    	if param[i].islower():
-            ret = ret + param[i].upper()
-    	else:
-            ret = ret + param[i].lower()
-                
-    return ret
-
+def hexadecimal(param):
+    return param
 
 
 def my_printf(format_string,param):
@@ -60,8 +11,8 @@ def my_printf(format_string,param):
     skip = 0
     for idx in range(0,len(format_string)):
         if skip == 0:
-            if format_string[idx] == '#' and format_string[idx+1] == 'g' and param.isnumeric():
-                print(invert_numbers(param),end="")
+            if format_string[idx] == '#' and format_string[idx+1] == 'j':
+                print(hexadecimal(param),end="")
                 skip = 1
             else:
                 print(format_string[idx],end="")
