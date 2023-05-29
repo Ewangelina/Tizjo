12d9d59b30055dfae9cd837270fdcf9d7ce885e9
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-05-29 17:49:35 +0200

    1

--- lab10/main.py
+++ lab10/main.py
@@ -2,28 +2,46 @@
 
 import sys
 
+def make_hex(x):
+    h = str(hex(x))
+    ret = ""
+    for i in range(2, len(h)):
+        ret += h[i]
+    
+    return ret
+
+def is_number(x):
+    return x.isnumeric()
+
+def no_digits(x):
+    ret = 0
+    for i in range(0, len(x)):
+        if is_number(x[i]):
+            ret += 1
+
 def getF(input):
+    o = int(input)
+    n = no_digits(input)
+    return int((o*2)/n))
 
+def do_print(input):
+    f = getF(input)
+    if f % 2 == 0:
+        return str(f)
+    else:
+        return make_hex(f)
 
+        
 def my_printf(format_string,param):
     #print(format_string)
     skip = 0
-    for idx in range(0,len(format_string)):
+    for idx in range(0,len(format_string) - 1):
         if skip == 0:
-            if format_string[idx] == '#' and format_string[idx+1] == '.' and is_number(format_string[idx+2]):
-                i = idx + 3
-                num = int(format_string[idx+2])
-                filler = "o"
-
-                while is_number(format_string[i]):
-                    num *= 10
-                    num += int(format_string[i])
-                    i += 1
-                
-                if format_string[i] == 'h':
-                    print(float_dot(param, num),end="")
-                    skip = i - idx   
-                else:
+            if format_string[idx] == '#' and format_string[idx+1] == 'a':
+                try:
+                    print(getF(param),end="")
+                    skip = 1  
+                except:
                     print(format_string[idx],end="")
             else:
                 print(format_string[idx],end="")
